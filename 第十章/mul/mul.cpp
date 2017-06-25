5d6b7b8f8fa571071bd0167f6c1b6db030736b3b
Jack4feng <jackfeng1998@gmail.com>
2017-06-25 22:49:26 +0800

    multip

--- 第十章/mul/mul.cpp
+++ 第十章/mul/mul.cpp
@@ -14,6 +14,21 @@ int main()
 	course.insert(make_pair("complier", "211-6"));
 	course.insert(make_pair("os", "2-67"));
 	course.insert(make_pair("os", "5-5"));
-
-
+	//输入一个课程，直到找到该课程为止，记下上课次数
+	string name;
+	int count;
+	do
+	{
+		cin >> name;
+		count = course.count(name);
+		if (count == 0)
+		{
+			cout << "can not find this class" << endl;
+		}
+	} while (count == 0);
+	pair<courseit, courseit>range = course.equal_range(name);//得到该课程对应的区间
+	for (multimap<string, string>::iterator it = range.first; it != range.second; it++)
+		//first指向区间头，second指向区间尾
+		cout << it->second << " " << endl;
+	return 0;
 }
